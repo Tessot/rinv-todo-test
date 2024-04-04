88619d956d37b656cd1dd52d54b5ac8531cc648f
Ozioma Uzoegwu <31167303+Tessot@users.noreply.github.com>
2024-04-04 15:28:10 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { List } from './components/List';
+
+function App() {
+
+  return (
+    <div>
+      <List />
+    </div>
+
+  );
+}
+
+export default App;
